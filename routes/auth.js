0e3bb690944c32c503f7767505f135d1964f720b
akash jha <akashjhasciencea@gmail.com>
2025-01-09 20:19:14 +0530

    token

--- routes/auth.js
+++ routes/auth.js
@@ -34,7 +34,7 @@ router.post('/login', async (req, res) => {
         }
 
         
-        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '7d' });
         res.json({ token, role: user.role});
     } catch (err) {
         res.status(500).json({ error: err.message });
